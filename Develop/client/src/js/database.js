15ab80d3e15e12bd40f4881a4be670e50d8dd6a4
hmustain <hunter@bunkerbranding.com>
2023-03-01 13:44:46 -0600

    todos on the database.js put and get methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,63 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // Create connection to the database and version we want to use
+    const contactDb = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges
+    const tx = contactDb.transaction('jate', 'readwrite');
+
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .add() method on the store and pass in the content
+    const request = store.add({ content });
+
+    // Get confirmation of the req
+    const result = await request;
+    console.log('data saved to the database', result);
+  } catch (error) {
+    console.error('Failed to save data to database', error);
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    // Create a connection to the database and version we want to use
+    const contactDb = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges
+    const tx = contactDb.transaction('jate', 'readonly');
+
+    // Open up the desired object store
+    const store = tx.objectStore('jate');
+
+    // Use the .getAll() method to get all data in the database
+    const request = store.getAll();
+
+    // Get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (error) {
+    console.error('Failed to get data from database', error);
+  }
+};
 
 initdb();
